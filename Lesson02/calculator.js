ae011252bab36918dfc8097bf747241a256ead16
Liz Finch <luna@lizs-imac.lan>
2021-01-21 21:51:55 -0900

    adding additional operators

--- Lesson02/calculator.js
+++ Lesson02/calculator.js
@@ -1,9 +1,9 @@
 let toddler = 3; // age 3
-let kid = 5;  // age 5
 
-let teen = toddler + (kid*2);
 
-let adult = teen + kid;
+let kid = 9 % toddler;  // age 5
+let teen = toddler + (kid*2); // age 13
+let adult = teen + kid; // age 18
 
 console.log(`Toddler: ${toddler}`)
 console.log(`Kid: ${kid}`)
