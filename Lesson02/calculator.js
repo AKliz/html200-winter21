5f70683ac3dfd5b449c9ecfe8fd1de9b15342c9a
Liz Finch <luna@lizs-imac.lan>
2021-01-21 21:21:49 -0900

    uploading age calculator

---
+++ Lesson02/calculator.js
@@ -0,0 +1,11 @@
+let toddler = 3; // age 3
+let kid = 5;  // age 5
+
+let teen = toddler + (kid*2);
+
+let adult = teen + kid;
+
+console.log(`Toddler: ${toddler}`)
+console.log(`Kid: ${kid}`)
+console.log(`Teenager: ${teen}`)
+console.log(`Adult: ${adult}`)
