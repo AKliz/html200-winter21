2584febfdf010402495702519f7759b2013834f2
Liz Finch <luna@lizs-imac.lan>
2021-01-23 13:33:57 -0900

    adding canDrive.js bonus project

---
+++ Lesson02/canDrive.js
@@ -0,0 +1,18 @@
+let firstName = "Liz";
+let lastName = "Cullivan";
+let age = 32;
+let legalAge = 16;
+
+let drivingYears = age - legalAge;
+
+if (age > legalAge){
+   console.log(`${firstName} ${lastName} can drive. They have been driving for ${drivingYears} years.`)
+}
+
+
+if(age < legalAge){
+  for (let i = 5; i > 0; i--){
+      console.log(i);
+    }
+
+}
