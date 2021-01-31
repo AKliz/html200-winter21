94b4988c2ae1f28bdf601f8c0e47fd5f6e059388
Liz Cullivan <lizotcweb@gmail.com>
2021-01-31 12:17:08 -0900

    adding lesson02-2 to lesson03a branch

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,11 @@
+//set string variable to be sentence of at least five words and a period.
+let comment = 'I love skiing in the winter.'
+console.log(comment);
+
+//make array from that sentence
+let activity = [];
+activity = comment.split(' ');
+console.log(activity);
+
+//console.log third word
+console.log(activity[2]);
