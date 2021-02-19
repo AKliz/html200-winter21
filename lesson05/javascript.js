87e4360ec6442f7d6e6fd79167a5ced9c573b60b
AKliz <lizotcweb@gmail.com>
2021-02-19 11:22:17 -0900

    adding in switches

--- lesson05/javascript.js
+++ lesson05/javascript.js
@@ -5,69 +5,78 @@ let testOverdrawl = 0;
 const withdraw = 'Enter an amount to widthdraw';
 const deposit = 'Enter an amount to deposit';
 const warning = 'Low balance warning - this will make your new balance less than $300. Do you want to proceed? Y or N';
+const welcomeMessage = 'Please choose an action: Q to quit; W to widthdraw; D to deposit; B to see balance.';
+const newBankBalance = 'New Bank Balance is'
 
-function bank(action) {
-  const welcomeMessage = 'Please choose an action: Q to quit; W to widthdraw; D to deposit; B to see balance.';
-  do{
+
+function bank(open) {
+  while (open === true){
     let welcome = prompt(welcomeMessage);
     console.log('starting banking app');
-    action = welcome.toUpperCase();
-    console.log('RUN ACTION ' + action);
+    let action = welcome.toUpperCase();
+    console.log(`RUN ACTION ${action}`);
     let money = 0;
-    if(action === 'W'){
+    switch(action){
+      case 'W':
         console.log('WITHDRAW');
         money = prompt(withdraw);
         money = Number(money);
         testOverdrawl = bankBalance - money;
         if (money < 0 || testOverdrawl <  0){
-          alert ("Warning! No Negative Withdraws or Balances Allowed");
+            alert ('Warning! No Negative Withdraws or Balances Allowed');
         }
         else if (testOverdrawl < 300){
           let proceed = prompt(warning);
-          console.log('change to upper case')
-          proceed.toUpperCase;
-          if (proceed === 'Y'){
-            bankBalance = bankBalance - money;
-            console.log('withdraw approved and removed');
-          }
-          else {
-            console.log('withdraw declined');
-            alert('Widthdraw Cancelled');
+          console.log('change to upper case');
+          switch (proceed.toUpperCase()) {
+            case 'N':
+              console.log(`withdraw declined - entry ${proceed}`);
+              alert(`Withdraw cancelled - Bank Balance remains at ${bankBalance}`);
+              break;
+            case 'Y':
+              bankBalance = bankBalance - money;
+              console.log('withdraw approved and removed');
+              alert(`New Bank Balance is ${bankBalance}`);
+              break;
+            default:
+              alert('Widthdraw Cancelled - invalid entry.');
+              console.log(`Invalid entry ${proceed} - withdraw cancelled`);
           }
         }
-        else{
+        else {
           bankBalance = bankBalance - money;
-          alert('New Bank Balance is ' + bankBalance);
+          alert(`${newBankBalance} ${bankBalance}`);
         }
-        console.log('New Bank Balance is ' + bankBalance);
-      }
-      else if (action === 'D'){
+        console.log(newBankBalance);
+        break;
+      case 'D':
         console.log('DEPOSIT');
         money = prompt(deposit);
         money = Number(money);
-         if (money > 50000){
-           alert ("Warning! You've entered an amount over our deposit cap of $50,000.")
-         }
-         else {
-           bankBalance = bankBalance + money;
-           alert('New Bank Balance is ' + bankBalance);
-           console.log('New Bank Balance is ' + bankBalance);
-         }
-      }
-      else if (action ===  'B'){
+        if(money > 50000){
+            console.log(`Bank deposit of ${money} declined - over $50,000`);
+            alert ("Warning! You've entered an amount over our deposit cap of $50,000.");
+        }
+        else{
+            bankBalance = bankBalance + money;
+            alert(`${newBankBalance} ${bankBalance}`);
+            console.log(`${newBankBalance} ${bankBalance}`);
+          }
+        break;
+      case 'B':
         console.log('BALANCE');
-        console.log('New Bank Balance is ' + bankBalance);
-        alert('New Bank Balance is ' + bankBalance);
-      }
-      else if (action !== 'Q'){
-        alert ('Wrong entry try again.')
-      }
-      else{
+        console.log(`${newBankBalance} ${bankBalance}`);
+        alert(`Current Balance is: ${bankBalance}`);
+        break;
+      case 'Q':
+        open = false;
+        alert(`EXITING - Current Balance is: ${bankBalance}`);
+        break;
+      default:
+        alert ('Wrong entry try again.');
         console.log('invalid entry - exiting');
-      }
-
-    } while (action !== 'Q');
-  console.log('END OF DO WHILE LOOP')
-  alert('EXITING - Your New Bank Balance is ' + bankBalance);
-  console.log ('END OF ACTION FUNCTION')
+        break;
+    }
+  }
+console.log('END OF DO WHILE LOOP & Bank Function');
 }
